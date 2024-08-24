873223d3d8bf79855f665a3dbfddb7829fb3babf
Chitrarth <chitrarthrai10@gmail.com>
2024-08-24 21:54:31 +0530

    Added feature to add products to cart

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -5,7 +5,7 @@ import { Header, PageLoader } from "components/commons";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, without } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
@@ -13,6 +13,7 @@ const ProductList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
+  const [cartItems, setCartItems] = useState([]);
 
   const [products, setProducts] = useState([]);
   const fetchProducts = async () => {
@@ -32,6 +33,13 @@ const ProductList = () => {
     fetchProducts();
   }, [debouncedSearchKey]);
 
+  const toggleIsInCart = slug =>
+    setCartItems(prevCartItems =>
+      prevCartItems.includes(slug)
+        ? without([slug], cartItems)
+        : [slug, ...cartItems]
+    );
+
   if (isLoading) {
     return <PageLoader />;
   }
@@ -41,6 +49,7 @@ const ProductList = () => {
   return (
     <div className="flex h-screen flex-col">
       <Header
+        cartItemsCount={cartItems.length}
         shouldShowBackButton={false}
         title="Smile cart"
         actionBlock={
@@ -58,7 +67,12 @@ const ProductList = () => {
       ) : (
         <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
           {products.map(product => (
-            <ProductListItem key={product.slug} {...product} />
+            <ProductListItem
+              key={product.slug}
+              {...product}
+              isInCart={cartItems.includes(product.slug)}
+              toggleIsInCart={() => toggleIsInCart(product.slug)}
+            />
           ))}
         </div>
       )}
