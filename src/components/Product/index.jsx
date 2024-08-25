ff1890ace986b1e1dabcbfe98ce54f2a63c6b673
Chitrarth <chitrarthrai10@gmail.com>
2024-08-25 19:03:27 +0530

    Added cart page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,31 +1,36 @@
 import { useState, useEffect } from "react";
 
+import productsApi from "apis/products";
 import {
   Header,
-  PageNotFound,
   PageLoader,
+  PageNotFound,
   AddToCart,
 } from "components/commons";
-import { Typography } from "neetoui";
+import useSelectedQuantity from "components/hooks/useSelectedQuantity";
+import { Typography, Button } from "neetoui";
 import { append, isNotNil } from "ramda";
 import { useParams } from "react-router-dom";
+import routes from "routes";
 
 import Carousel from "./Carousel";
 
-import productsApi from "../../apis/products";
-
 const Product = () => {
-  const { slug } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-  const [product, setProduct] = useState({});
   const [isError, setIsError] = useState(false);
+  const [product, setProduct] = useState({});
+
+  const { slug } = useParams();
+
+  const { selectedQuantity, setSelectedQuantity } = useSelectedQuantity(slug);
 
   const fetchProduct = async () => {
     try {
       const response = await productsApi.show(slug);
       setProduct(response);
-    } catch {
+    } catch (error) {
       setIsError(true);
+      console.log("An error occurred:", error);
     } finally {
       setIsLoading(false);
     }
@@ -35,7 +40,15 @@ const Product = () => {
     fetchProduct();
   }, []);
 
-  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const {
+    name,
+    description,
+    mrp,
+    offerPrice,
+    imageUrls,
+    imageUrl,
+    availableQuantity,
+  } = product;
 
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
@@ -50,33 +63,38 @@ const Product = () => {
     <>
       <Header title={name} />
       <div className="mt-16 flex gap-4">
-        <div className="mt-16 flex gap-4">
-          <div className="w-2/5">
-            <div className="flex justify-center gap-16">
-              {isNotNil(imageUrls) ? (
-                <Carousel
-                  imageUrls={append(imageUrl, imageUrls)}
-                  title={name}
-                />
-              ) : (
-                <img alt={name} className="w-48" src={imageUrl} />
-              )}
-            </div>
+        <div className="w-2/5">
+          <div className="flex justify-center gap-16">
+            {isNotNil(imageUrls) ? (
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+            ) : (
+              <img alt={name} className="w-48" src={imageUrl} />
+            )}
           </div>
-          <div className="w-3/5 space-y-4">
-            <Typography>{description}</Typography>
-            <Typography>MRP: {mrp}</Typography>
-            <Typography className="font-semibold">
-              Offer price: {offerPrice}
-            </Typography>
-            <Typography className="font-semibold text-green-600">
-              {discountPercentage}% off
-            </Typography>
-            <AddToCart {...{ slug }} />
+        </div>
+        <div className="w-3/5 space-y-4">
+          <Typography>{description}</Typography>
+          <Typography>MRP: {mrp}</Typography>
+          <Typography className="font-semibold">
+            Offer price: {offerPrice}
+          </Typography>
+          <Typography className="font-semibold text-green-600">
+            {discountPercentage}% off
+          </Typography>
+          <div className="flex space-x-10">
+            <AddToCart {...{ availableQuantity, slug }} />
+            <Button
+              className="bg-neutral-800 hover:bg-neutral-950"
+              label="Buy now"
+              size="large"
+              to={routes.checkout}
+              onClick={() => setSelectedQuantity(selectedQuantity || 1)}
+            />
           </div>
         </div>
       </div>
     </>
   );
 };
+
 export default Product;
