6164081e4dc9c8fce9346fa4d031165cc5909d4d
Chitrarth <chitrarthrai10@gmail.com>
2024-08-27 15:17:43 +0530

    Adjusted ProductList component to use query parameter info

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
+import { filterNonNull } from "@bigbinary/neeto-cist";
 import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
-import useDebounce from "hooks/useDebounce";
+import useFuncDebounce from "hooks/useFuncDebounce";
+import useQueryParams from "hooks/useQueryParams";
 import { Search } from "neetoicons";
 import { Input, NoData, Pagination } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, mergeLeft } from "ramda";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 import withTitle from "utils/withTitle";
 
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
@@ -15,15 +20,38 @@ import ProductListItem from "./ProductListItem";
 const ProductList = () => {
   const { t } = useTranslation();
 
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
-  const [searchKey, setSearchKey] = useState("");
-  const debouncedSearchKey = useDebounce(searchKey);
+  const history = useHistory();
+
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
+
+  const [searchKey, setSearchKey] = useState(searchTerm);
 
   const productsParams = {
-    searchTerm: debouncedSearchKey,
-    page: currentPage,
-    pageSize: DEFAULT_PAGE_SIZE,
+    searchTerm,
+    page: Number(page) || DEFAULT_PAGE_INDEX,
+    pageSize: Number(pageSize) || DEFAULT_PAGE_SIZE,
   };
+
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null,
+    };
+
+    setSearchKey(value);
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)));
+  });
+
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(
+        routes.products.index,
+        mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }, queryParams)
+      )
+    );
 
   const { data: { products = [], totalProductsCount } = {}, isLoading } =
     useFetchProducts(productsParams);
@@ -45,9 +73,9 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={event => {
-              setSearchKey(event.target.value);
-              setCurrentPage(DEFAULT_PAGE_INDEX);
+            onChange={({ target: { value } }) => {
+              updateQueryParams(value);
+              setSearchKey(value);
             }}
           />
         }
@@ -64,9 +92,9 @@ const ProductList = () => {
       <div className="mb-5 self-end">
         <Pagination
           count={totalProductsCount}
-          navigate={page => setCurrentPage(page)}
-          pageNo={currentPage || DEFAULT_PAGE_INDEX}
-          pageSize={DEFAULT_PAGE_SIZE}
+          navigate={handlePageNavigation}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize) || DEFAULT_PAGE_SIZE}
         />
       </div>
     </div>
